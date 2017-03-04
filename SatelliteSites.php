34d1209cefea7306424556dbdc2cdfded0acda43
Isaac Overlander <Isaac.overlander@gmail.com>
2017-03-04 08:43:39 -0600

    Initial Setup

---
+++ SatelliteSites.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin Name: Satelite Site Identifier
+Plugin URI: http://overinkd.com/satellite-sites
+Description: This plugin provides the connection between a Memberpress Membership and an alternate domain on which to automatically setup a subdomain site for that member. Requirements: Memberpress, and Memberpress Developer add-on installed on the site where this plug in resides; and NS Site Cloner and WP-CLI installed on the remote wordpress install and server.
+Version: 0.17.0304.1
+Author: Isaac Overlander
+Author URI: http://overinkd.com
+License: GPL3
+
+Copyright 2017  Isaac Overlander  (email : admin@overinkd.com)
+*/
+
+
+?>
